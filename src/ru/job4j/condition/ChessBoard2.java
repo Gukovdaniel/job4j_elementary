f1f30ad2683957fbd468c92d86bcb31e7bee47ec
Gukovdaniel <gukov.danila@bk.ru>
2020-12-28 14:03:41 +0300

    Math.abs [#431950]

--- src/ru/job4j/condition/ChessBoard2.java
+++ src/ru/job4j/condition/ChessBoard2.java
@@ -1,6 +1,6 @@
 package ru.job4j.condition;
 
-public class ChessBoard {
+public class ChessBoard2 {
     public static int way(int x1, int y1, int x2, int y2) {
         return Math.abs(y2 - y1) == Math.abs(x2 - x1) ? Math.abs(y2 - y1) : 0;
     }
--- test/ru/job4j/condition/ChessBoard2Test.java
+++ test/ru/job4j/condition/ChessBoard2Test.java
@@ -5,22 +5,22 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class ChessBoardTest {
+public class ChessBoard2Test {
     @Test
     public void wayIs5() {
-        int way = ChessBoard.way(6, 7, 1, 2);
+        int way = ChessBoard2.way(6, 7, 1, 2);
         assertThat(way, is(5));
     }
 
     @Test
     public void wayIs7() {
-        int way = ChessBoard.way(7, 0, 0, 7);
+        int way = ChessBoard2.way(7, 0, 0, 7);
         assertThat(way, is(7));
     }
 
     @Test
     public void wayIs0() {
-        int way = ChessBoard.way(2, 6, 4, 1);
+        int way = ChessBoard2.way(2, 6, 4, 1);
         assertThat(way, is(0));
     }
 }
