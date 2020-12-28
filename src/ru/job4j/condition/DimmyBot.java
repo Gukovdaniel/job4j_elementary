969ea438e9f9d74bbf5dd97ef1a1496744c939ff
Gukovdaniel <gukov.danila@bk.ru>
2020-12-28 12:57:56 +0300

    Множественное логическое выражение И [#431954]

--- src/ru/job4j/condition/DimmyBot.java
+++ src/ru/job4j/condition/DimmyBot.java
@@ -3,6 +3,7 @@ package ru.job4j.condition;
 class DummyBot {
 
     public static String answer(String question) {
+        System.out.println(question);
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             /* заменить ... на правильный ответ rsl = "ответ по заданию". */
--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,13 +1,13 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static String liken(int a, int b) {
-        String num = a > b ? "a" : "b";
-        return num;
+    public static int max(int left, int right) {
+        int result = left;
+        return result;
     }
 
     public static void main(String[] args) {
-        String msg = Max.liken(1, 3);
+        String msg = Max.max(1, 3);
         System.out.println(msg);
     }
 }
