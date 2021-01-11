59e376c4991db642faf7e5b03bf8119e00e4092a
Gukovdaniel <gukov.danila@bk.ru>
2021-01-11 19:35:19 +0300

    Множественное логическое выражение И [#431954]

--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -13,7 +13,7 @@ public class TriangleTest {
 
     @Test
     public void whenNotExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        boolean result = Triangle.exist(2.0, 2.0, 6.0);
         assertThat(result, is(false));
     }
 }
