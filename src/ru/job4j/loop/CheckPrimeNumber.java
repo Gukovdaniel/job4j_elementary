e5252d6b0af3dbfb5624d7a85f87583e7c55edac
Gukovdaniel <gukov.danila@bk.ru>
2021-01-14 00:14:25 +0300

    Простое число [#432051]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,28 +2,15 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number == 1){
-            prime = false;
-            System.out.println(prime);
-        } else {
-            for (int i = 2; i < number; i++) {
-                if (number == 1) {
-                    prime = false;
-                    System.out.println(prime);
-                    break;
-                }
+        boolean prime = number > 1;
+        for (int i = 2; i < number; i++) {
                 if (number % i == 0) {
                     prime = false;
                     System.out.println(prime);
                     break;
                 }
-                prime = true;
-                System.out.println(prime);
-                break;
-            }
         }
-            return prime;
+        return prime;
     }
 
 }
