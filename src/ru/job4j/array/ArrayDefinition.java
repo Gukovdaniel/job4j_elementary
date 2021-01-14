1195483f6338470073008d9e84be3db7855cc697
Gukovdaniel <gukov.danila@bk.ru>
2021-01-14 17:16:58 +0300

    Заполнение массива. [#431972]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,7 +6,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива " + ages.length);
         String[] surnames = new String[100500];
         System.out.println("Размер массива " + surnames.length);
-        float [] prices = new float[40];
+        float[] prices = new float[40];
         System.out.println("Размер массива " + prices.length);
-            }
+        String[] names = new String[4];
+        names[0] = "Ярослава";
+        names[1] = "Мария";
+        names[2] = "Настя";
+        names[3] = "Ира";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+    }
 }
